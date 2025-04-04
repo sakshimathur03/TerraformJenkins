13336f37fd109aff869dc5eaefe1a17e585120ec
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-04 16:29:47 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "myAppService"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+}
