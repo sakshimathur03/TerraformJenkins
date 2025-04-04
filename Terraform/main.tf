d542b2f76abc08f5fb29765ee550167aa86cabeb
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-04 16:54:41 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,28 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "2f1ef4d2-8798-474f-81d1-d2fc16c553b6"
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_app_service_plan" "asp" {
-  name                = "myAppServicePlan"
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
   reserved            = true
+
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Standard"
+    size = "S1"
   }
 }
 
+
+
+# App Service
 resource "azurerm_app_service" "app" {
-  name                = "myAppService"
+  name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+}
+
+# Deployment Slot (Fixed)
+resource "azurerm_app_service_slot" "slot" {
+  name                = var.deployment_slot_name
+  app_service_name    = azurerm_app_service.app.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  depends_on = [azurerm_app_service.app]
 }
