123e756994c98db7f0a824896cd7769e11cc5766
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-05 10:28:41 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,40 +1,39 @@
 provider "azurerm" {
   features {}
-  subscription_id = "3354a426-5b85-4323-9efe-9581e63d9653"
+  subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "rgasp" {
   location = var.location
+  name = var.resource_group_name
+  
+}
+data "azurerm_client_config" "name" {}
+
+resource "azurerm_service_plan" "plan" {
+  location = azurerm_resource_group.rgasp.location
+  name = var.service_plan_name
+  os_type = var.os
+  resource_group_name = azurerm_resource_group.rgasp.name
+  sku_name = var.pricing_plan
 }
 
-# Add a delay to allow Azure to propagate the resource group creation
-resource "time_sleep" "wait_after_rg" {
-  depends_on = [azurerm_resource_group.rg]
-  create_duration = "30s"
-}
-
-resource "azurerm_service_plan" "app_service_plan" {
-  name                = var.service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = var.os_type
-  sku_name            = var.sku_name
-
-  depends_on = [time_sleep.wait_after_rg]
-}
-
-resource "azurerm_linux_web_app" "app" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.app_service_plan.id
-
+resource "azurerm_windows_web_app" "serviceApp" {
+  service_plan_id = azurerm_service_plan.plan.id
+  location = azurerm_service_plan.plan.location
+  name = var.linux_web_app_name
+  resource_group_name = azurerm_resource_group.rgasp.name
   site_config {
-    always_on = true
-  }
+    application_stack {
+      dotnet_version = "v8.0"
+    }
+}
+    
+}
 
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+resource "azurerm_windows_web_app_slot" "deploySlot" {
+  app_service_id = azurerm_windows_web_app.serviceApp.id
+  name = var.linux_web_app_slot_name
+  site_config {
   }
 }
