ae1b5aace29b9be6d6e37114966d441155e12463
Sakshi Mathur <147843432+sakshimathur03@users.noreply.github.com>
2025-04-05 09:06:38 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,34 +2,30 @@ provider "azurerm" {
   features {}
 }
 
-# Define the Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-# Define the Service Plan (Fixed Missing Arguments)
-resource "azurerm_service_plan" "plan" {
-  name                = "myAppServicePlan"
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = var.service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"    # Change to "Windows" if needed
-  sku_name            = "B1"       # Example SKU: F1 (Free), B1 (Basic), P1V2 (Premium)
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
-# Define the App Service
-resource "azurerm_linux_web_app" "app" {
-  name                = "myWebApp"
+resource "azurerm_app_service" "app" {
+  name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
 
   site_config {
     always_on = true
   }
-}
 
-# Output App Service URL
-output "app_service_url" {
-  value = azurerm_linux_web_app.app.default_site_hostname
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
 }
